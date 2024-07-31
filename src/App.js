3f452e8a98d1c370278610fa09d070c6d0ac77be
siphokazi <naledimuhauli@gmail.com>
2024-07-31 19:09:39 +0200

    added header of the page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+     <h1 className='heading1'>Cooking <br/> Book</h1>
+     <p className='paragraph1'>By Siphokazi and Wakhekile</p>
     </div>
+    </div>
+    </>
   );
 }
 
