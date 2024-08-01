ff105d9174dc7cb923bd399dee747e5231c96868
siphokazi <naledimuhauli@gmail.com>
2024-08-01 04:14:11 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RecipeList from './recipe-list';
+import Navbar from './Navbar';
 
 
 function App() {
   return (
     <>
     <div className="App">
+      <Navbar />
       <div className='header'>
      <h1 className='heading1'>Cooking <br/> Book</h1>
      <p className='paragraph1'>By Siphokazi and Wakhekile</p>
---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Navbar = () =>{
+    return(
+        <div className="container">
+        <header class="d-flex justify-content-center py-3">
+          <ul className="nav nav-pills">
+            <li className="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+            <li className="nav-item"><a href="#" class="nav-link">About Us</a></li>
+            <li className="nav-item"><a href="#" class="nav-link">Savoury Recipes</a></li>
+            <li className="nav-item"><a href="#" class="nav-link">Sweet Recipes</a></li>
+            <li classNameS="nav-item"><a href="#" class="nav-link">Contact Us</a></li>
+          </ul>
+        </header>
+      </div>
+    )
+}
+
+export default Navbar
